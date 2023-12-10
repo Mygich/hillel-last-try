1b978c6b49cd3c72044286fa7d315025a55a11c0
Lada Antsyferova <mygi746@gmail.com>
2023-12-10 02:42:55 +0200

    menu

---
+++ Lesson-18/class/script.js
@@ -0,0 +1,5 @@
+document
+         .querySelector(".burger-menu")
+         .addEventListener("click", function () {
+         document .querySelector(".menu") .classList.toggle("active")
+         })
---
+++ Lesson-18/hw/script.js
@@ -0,0 +1,10 @@
+document.getElementById("my-button")
+   .addEventListener("click", function () {
+      document.getElementById("my-text").style.display = "block";
+       document.getElementById("my-text-add").style.display = "block";
+   });
+   document.getElementById("add-button")
+   .addEventListener("click", function () {
+      document.getElementById("add-text").style.display = "block";
+       document.getElementById("add-text-2").style.display = "block";
+   });
